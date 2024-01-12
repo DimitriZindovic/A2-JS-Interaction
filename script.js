8115d663e7a0df0ad33f4de24ceddb3d73796473
Dimitri Zindovic <dimitri.zindovic2@gmail.com>
2024-01-12 12:56:27 +0100

    :hammer :

---
+++ script.js
@@ -0,0 +1,125 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const lastSearchedPokemon = localStorage.getItem("lastSearchedPokemon");
+  if (lastSearchedPokemon) {
+    document.querySelector(".inputResearch").value = lastSearchedPokemon;
+  }
+  document.querySelector(".buttonResearch").addEventListener("click", () => {
+    const userInput = document
+      .querySelector(".inputResearch")
+      .value.toLowerCase();
+    localStorage.setItem("lastSearchedPokemon", userInput);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Aucun Pokémon nommé "${userInput}" trouvé.`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        localStorage.setItem("pokemonName", data.name);
+        localStorage.setItem("pokemonImage", data.sprites.front_default);
+        localStorage.setItem("pokemonStats", JSON.stringify(data.stats));
+
+        window.location.href = "product.html";
+      })
+      .catch((error) => {
+        const searchBarContainer = document.querySelector("h2");
+        const errorMessage = document.createElement("p");
+        errorMessage.classList.add("error");
+        errorMessage.textContent = error.message;
+        searchBarContainer.insertAdjacentElement("afterend", errorMessage);
+      });
+  });
+});
+
+const pokemonName = localStorage.getItem("pokemonName");
+const pokemonImage = localStorage.getItem("pokemonImage");
+const pokemonStats = JSON.parse(localStorage.getItem("pokemonStats"));
+
+const pokemonNameCapitalized =
+  pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+
+const pokemonPrice = Math.floor(Math.random() * 2000);
+
+const cardPokemon = document.querySelector(".card-pokemon");
+if (cardPokemon) {
+  cardPokemon.innerHTML = `
+      <img class="card-image" src="${pokemonImage}" alt="${pokemonNameCapitalized}" />
+      <div class="description-pokemon">
+        <h3>${pokemonNameCapitalized}</h3>
+        <ul>
+          ${pokemonStats
+            .map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`)
+            .join("")}
+        </ul>
+        <p>Prix: ${pokemonPrice}€</p>
+        <button class="button-add-cart">Ajouter au panier  →</button>
+      </div>
+    `;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const cartContainer = document.querySelector(".cart-container");
+
+  cart.forEach((pokemon) => {
+    const pokemonCard = document.createElement("div");
+    pokemonCard.innerHTML = `
+    <div class="list-pokemon">
+      <img src="${pokemon.image}" alt="${pokemon.name}" />
+      <div class="description-product">
+        <h4>${pokemon.name}</h4>
+        <p>Prix: ${pokemon.price}€</p>
+      </div>
+    </div>
+    `;
+    if (cartContainer) {
+      cartContainer.appendChild(pokemonCard);
+    }
+  });
+
+  const totalPrice = cart.reduce((total, pokemon) => total + pokemon.price, 0);
+  const totalPriceElement = document.createElement("p");
+  totalPriceElement.classList.add("total-price");
+  totalPriceElement.textContent = `Prix total: ${totalPrice}€`;
+  if (cartContainer) {
+    cartContainer.appendChild(totalPriceElement);
+  }
+
+  const addToCartButton = document.querySelector(".button-add-cart");
+  if (addToCartButton) {
+    addToCartButton.addEventListener("mouseover", () => {
+      anime({
+        targets: addToCartButton,
+        scale: [1, 1.1, 1],
+        duration: 1000,
+        easing: "easeInOutQuad",
+        loop: true,
+      });
+    });
+
+    addToCartButton.addEventListener("click", () => {
+      const cart = JSON.parse(localStorage.getItem("cart")) || [];
+      const pokemon = {
+        name: pokemonNameCapitalized,
+        image: pokemonImage,
+        price: pokemonPrice,
+      };
+
+      cart.push(pokemon);
+      localStorage.setItem("cart", JSON.stringify(cart));
+
+      window.location.href = "cart.html";
+    });
+  }
+});
+
+const cardImage = document.querySelector(".card-image");
+if (cardImage) {
+  anime({
+    targets: cardImage,
+    rotate: "1turn",
+    duration: 1000,
+    easing: "easeInOutSine",
+  });
+}
