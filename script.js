5e82f60639a288514dfd891078f86d26446c9a43
Dimitri Zindovic <dimitri.zindovic2@gmail.com>
2024-01-14 17:09:10 +0100

    :hammer: NEW animation

--- script.js
+++ script.js
@@ -1,83 +1,81 @@
 document.addEventListener("DOMContentLoaded", () => {
+  setLastSearchedPokemon();
+  handleResearchButtonClick();
+  displayCartItems();
+  animateCardImage();
+});
+
+function setLastSearchedPokemon() {
   const lastSearchedPokemon = localStorage.getItem("lastSearchedPokemon");
   if (lastSearchedPokemon) {
     document.querySelector(".inputResearch").value = lastSearchedPokemon;
   }
+}
+
+function handleResearchButtonClick() {
   document.querySelector(".buttonResearch").addEventListener("click", () => {
     const userInput = document
       .querySelector(".inputResearch")
       .value.toLowerCase();
     localStorage.setItem("lastSearchedPokemon", userInput);
-    fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Aucun Pokémon nommé "${userInput}" trouvé.`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        localStorage.setItem("pokemonName", data.name);
-        localStorage.setItem("pokemonImage", data.sprites.front_default);
-        localStorage.setItem("pokemonStats", JSON.stringify(data.stats));
-
-        window.location.href = "product.html";
-      })
-      .catch((error) => {
-        const searchBarContainer = document.querySelector("h2");
-        const errorMessage = document.createElement("p");
-        errorMessage.classList.add("error");
-        errorMessage.textContent = error.message;
-        searchBarContainer.insertAdjacentElement("afterend", errorMessage);
-      });
+    fetchPokemon(userInput);
   });
-});
-
-const pokemonName = localStorage.getItem("pokemonName");
-const pokemonImage = localStorage.getItem("pokemonImage");
-const pokemonStats = JSON.parse(localStorage.getItem("pokemonStats"));
-
-const pokemonNameCapitalized =
-  pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
-
-const pokemonPrice = Math.floor(Math.random() * 2000);
-
-const cardPokemon = document.querySelector(".card-pokemon");
-if (cardPokemon) {
-  cardPokemon.innerHTML = `
-      <img class="card-image" src="${pokemonImage}" alt="${pokemonNameCapitalized}" />
-      <div class="description-pokemon">
-        <h3>${pokemonNameCapitalized}</h3>
-        <ul>
-          ${pokemonStats
-            .map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`)
-            .join("")}
-        </ul>
-        <p>Prix: ${pokemonPrice}€</p>
-        <button class="button-add-cart">Ajouter au panier  →</button>
-      </div>
-    `;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+function fetchPokemon(userInput) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Aucun Pokémon nommé "${userInput}" trouvé.`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      savePokemonData(data);
+      window.location.href = "product.html";
+    })
+    .catch(displayError);
+}
+
+function savePokemonData(data) {
+  localStorage.setItem("pokemonName", data.name);
+  localStorage.setItem("pokemonImage", data.sprites.front_default);
+  localStorage.setItem("pokemonStats", JSON.stringify(data.stats));
+}
+
+function displayError(error) {
+  const searchBarContainer = document.querySelector("h2");
+  const errorMessage = document.createElement("p");
+  errorMessage.classList.add("error");
+  errorMessage.textContent = error.message;
+  searchBarContainer.insertAdjacentElement("afterend", errorMessage);
+}
+
+function displayCartItems() {
   const cart = JSON.parse(localStorage.getItem("cart")) || [];
   const cartContainer = document.querySelector(".cart-container");
 
   cart.forEach((pokemon) => {
     const pokemonCard = document.createElement("div");
     pokemonCard.innerHTML = `
-    <div class="list-pokemon">
-      <img src="${pokemon.image}" alt="${pokemon.name}" />
-      <div class="description-product">
-        <h4>${pokemon.name}</h4>
-        <p>Prix: ${pokemon.price}€</p>
+      <div class="list-pokemon">
+        <img src="${pokemon.image}" alt="${pokemon.name}" />
+        <div class="description-product">
+          <h4>${pokemon.name}</h4>
+          <p>Prix: ${pokemon.price}€</p>
+        </div>
       </div>
-    </div>
-    `;
+      `;
     if (cartContainer) {
       cartContainer.appendChild(pokemonCard);
     }
   });
 
+  displayTotalPrice(cart, cartContainer);
+  handleAddToCartButtonClick();
+}
+
+function displayTotalPrice(cart, cartContainer) {
   const totalPrice = cart.reduce((total, pokemon) => total + pokemon.price, 0);
   const totalPriceElement = document.createElement("p");
   totalPriceElement.classList.add("total-price");
@@ -85,41 +83,81 @@ document.addEventListener("DOMContentLoaded", () => {
   if (cartContainer) {
     cartContainer.appendChild(totalPriceElement);
   }
+}
 
+function handleAddToCartButtonClick() {
   const addToCartButton = document.querySelector(".button-add-cart");
   if (addToCartButton) {
-    addToCartButton.addEventListener("mouseover", () => {
-      anime({
-        targets: addToCartButton,
-        scale: [1, 1.1, 1],
-        duration: 1000,
-        easing: "easeInOutQuad",
-        loop: true,
-      });
-    });
-
-    addToCartButton.addEventListener("click", () => {
-      const cart = JSON.parse(localStorage.getItem("cart")) || [];
-      const pokemon = {
-        name: pokemonNameCapitalized,
-        image: pokemonImage,
-        price: pokemonPrice,
-      };
-
-      cart.push(pokemon);
-      localStorage.setItem("cart", JSON.stringify(cart));
-
-      window.location.href = "cart.html";
-    });
+    addToCartButton.addEventListener("mouseover", animateButton);
+    addToCartButton.addEventListener("click", addToCart);
   }
-});
+}
 
-const cardImage = document.querySelector(".card-image");
-if (cardImage) {
+function animateButton() {
   anime({
-    targets: cardImage,
-    rotate: "1turn",
+    targets: this,
+    scale: [1, 1.1, 1],
     duration: 1000,
-    easing: "easeInOutSine",
+    easing: "easeInOutQuad",
+    loop: true,
   });
 }
+
+function addToCart() {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const pokemon = {
+    name: capitalizeFirstLetter(localStorage.getItem("pokemonName")),
+    image: localStorage.getItem("pokemonImage"),
+    price: Math.floor(Math.random() * 2000),
+  };
+
+  cart.push(pokemon);
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  window.location.href = "cart.html";
+}
+
+function animateCardImage() {
+  const cardImage = document.querySelector(".card-image");
+  if (cardImage) {
+    anime({
+      targets: cardImage,
+      rotate: "1turn",
+      duration: 1000,
+      easing: "easeInOutSine",
+    });
+  }
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function fetchRandomPokemonSprite() {
+  const randomPokemonId = Math.floor(Math.random() * 898) + 1;
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Aucun Pokémon avec l'ID "${randomPokemonId}" trouvé.`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const spriteElement = document.getElementById("random-pokemon-sprite");
+      spriteElement.style.backgroundImage = `url(${data.sprites.front_default})`;
+
+      anime({
+        targets: spriteElement,
+        rotate: "1turn",
+        scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
+        loop: true,
+        direction: "alternate",
+        duration: 2000,
+        easing: "easeInOutSine",
+      });
+    })
+    .catch(displayError);
+}
+
+window.onload = fetchRandomPokemonSprite;
